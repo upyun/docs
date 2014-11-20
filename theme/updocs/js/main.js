7ec0446b61a3cbe677d3ccfad665f513a740321c
Leigh Zhu <i@zhuli.me>
2014-11-20 21:14:18 +0800

    theme: add go-top

--- theme/updocs/js/main.js
+++ theme/updocs/js/main.js
@@ -64,4 +64,29 @@
     console.log('UPYUN loves you！ :)');
   });
 
+  // go-top
+  function goTop() {
+    $(window).scroll(function(e) {
+      if ($(window).scrollTop() > 200) {
+        $("#go-top").fadeIn(1000);
+      } else {
+        $("#go-top").fadeOut(1000);
+      }
+    });
+  }
+
+  $(function(){
+    $("#go-top").click(function(e) {
+      $('body, html').animate({
+        scrollTop: 0
+      }, 1000);
+    });
+    goTop();
+  });
+
+  // init tooltip plugin
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
 }(jQuery);
